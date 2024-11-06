3f377fdf4a24ef1f5b68b223b85ee8ea70832e13
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2024-11-06 15:20:25 +0900

    24/11/06 init

---
+++ Artifacts/HDI_Charts_swift.xcframework/ios-arm64/HDI_Charts_swift.framework/Headers/HDI_Charts_swift.h
@@ -0,0 +1,18 @@
+//
+//  HDI_Charts_swift.h
+//  HDI_Charts_swift
+//
+//  Created by 성재 on 11/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HDI_Charts_swift.
+FOUNDATION_EXPORT double HDI_Charts_swiftVersionNumber;
+
+//! Project version string for HDI_Charts_swift.
+FOUNDATION_EXPORT const unsigned char HDI_Charts_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDI_Charts_swift/PublicHeader.h>
+
+
--- Package.swift
+++ Package.swift
@@ -5,17 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "ChartKitX",
+    platforms: [.iOS(.v18)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ChartKitX",
-            targets: ["ChartKitX"]),
+            targets: ["ChartKitX", "HDI_Charts_swift"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ChartKitX"),
-
+        .binaryTarget(
+            name: "HDI_Charts_swift",
+            path: "Artifacts/HDI_Charts_swift.xcframework")
     ]
 )
